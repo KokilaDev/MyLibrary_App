9aaf389abf05caf3465d9f822773f0084f9b5e2a
KokilaDev <kokiladewmini527@gmail.com>
2026-07-09 11:50:32 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native"
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Text>Hello, My First Mobile App</Text>
+    </View>
+  )
+}
+
+export default App
