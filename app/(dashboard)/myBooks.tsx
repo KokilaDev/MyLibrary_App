f3a2dfd0745acfb98592fd3ce8a4340a027e7efb
KokilaDev <kokiladewmini527@gmail.com>
2026-07-12 20:01:37 +0530

    update myBook screen UI

--- app/(dashboard)/myBooks.tsx
+++ app/(dashboard)/myBooks.tsx
@@ -1,5 +1,5 @@
-import { Link, useRouter } from "expo-router"
-import { BookMarked, Plus } from "lucide-react-native";
+import { useRouter } from "expo-router"
+import { BookMarked, Pen, Plus, Trash2 } from "lucide-react-native";
 import { View, Text, ScrollView, StyleSheet, TouchableOpacity, Image } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -8,28 +8,28 @@ const MyBooks = () => {
     {
         id: 1,
         title: "The Great Gatsby",
-        writeAt: "2026-01-15",
+        category: "Novel",
         description: "A classic novel set in the 1920s, exploring themes of wealth, love, and the American Dream.",
         image: "https://images.unsplash.com/photo-1524995997946-a1c2e315a42f",
     },
     {
         id: 2,
         title: "To Kill a Mockingbird",
-        writeAt: "2026-02-10",
+        category: "Novel",
         description: "A powerful story about racial injustice and moral growth in the American South.",
         image: "https://images.unsplash.com/photo-1524995997946-a1c2e315a42f",
     },
     {
         id: 3,
         title: "1984",
-        writeAt: "2026-03-05",
+        category: "Dystopian",
         description: "A dystopian novel that delves into the dangers of totalitarianism and extreme political ideology.",
         image: "https://images.unsplash.com/photo-1524995997946-a1c2e315a42f",
     },
     {
         id: 4,
         title: "Pride and Prejudice",
-        writeAt: "2026-04-20",
+        category: "Romance",
         description: "A romantic novel that explores themes of love, social class, and personal growth in 19th-century England.",
         image: "https://images.unsplash.com/photo-1524995997946-a1c2e315a42f",
     }
@@ -72,8 +72,19 @@ const MyBooks = () => {
                   />
                 </View>
                 <View style={styles.bookInfo}>
-                  <Text style={styles.bookTitle}>{book.title}</Text>
-                  <Text style={styles.bookAuthor}>{book.writeAt}</Text>
+                  <View>
+                    <Text style={styles.bookTitle}>{book.title}</Text>
+                    <Text style={styles.bookCategory}>{book.category}</Text>
+                  </View>
+                  <View style={styles.divider}></View>
+                  <View style={styles.buttonContainer}>
+                    <TouchableOpacity style={styles.button}>
+                      <Pen size={14} color="#999999" />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.button}>
+                      <Trash2 size={14} color="#999999" />
+                    </TouchableOpacity>
+                  </View>
                 </View>
               </TouchableOpacity>
             ))}
@@ -145,33 +156,73 @@ const styles = StyleSheet.create({
     fontFamily: 'Poppins-Regular',
   },
   myBookContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: "space-between",
-    marginVertical: 15,
-    marginHorizontal: 8,
+    marginBottom: 20,
   },
   bookCard: {
-    width: 120,
-    marginRight: 16,
-    marginBottom: 24,
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#ffffff",
+    borderRadius: 18,
+    padding: 12,
+    marginBottom: 14,
+    borderWidth: 1,
+    borderColor: "#ffffff",
+    shadowColor: "#000",
+    shadowOffset: {
+        width: 0,
+        height: 2,
+    },
+    shadowOpacity: 0.06,
+    shadowRadius: 6,
+    elevation: 3,
   },
   bookCover: {
-    width: 120,
-    height: 180,
+    width: 58,
+    height: 84,
     backgroundColor: '#191919',
     borderRadius: 8,
-    marginBottom: 8,
+    marginRight: 14,
   },
   bookImage: {
     width: '100%',
     height: '100%',
     borderRadius: 8,
   },
-  bookInfo: {},
-  bookTitle: {},
-  bookAuthor: {},
-  bookDescription: {},
+  bookInfo: {
+    flex: 1,
+    justifyContent: "space-between",
+    // height: 84,
+  },
+  bookTitle: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "#1A181B",
+    marginBottom: 4,
+  },
+  bookCategory: {
+    fontSize: 14,
+    color: "#666",
+    fontFamily: "Poppins-Regular",
+    marginBottom: 4,
+  },
+  divider: {
+    height: 1,
+    backgroundColor: '#e5e5e5',
+    marginVertical: 4,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: "#e5e5e5",
+    borderRadius: 6,
+    padding: 6,
+    marginHorizontal: 8,
+    marginVertical: 4,
+  },
 })
 
 export default MyBooks
