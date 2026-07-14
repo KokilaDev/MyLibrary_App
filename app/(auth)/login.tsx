641ddef739a64c8a3b0fc0d9b4f99895908f536d
KokilaDev <kokiladewmini527@gmail.com>
2026-07-15 02:53:20 +0530

    update auth UIs

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,8 +1,10 @@
 import { useLoader } from "@/hooks/useLoader";
 import { login } from "@/services/authService";
 import { router } from "expo-router"
+import { BookText, Eye, LockIcon, Mail } from "lucide-react-native";
 import { useState } from "react";
-import { View, Text, Alert, TextInput, Keyboard, TouchableWithoutFeedback, Pressable, TouchableOpacity } from "react-native"
+import { View, Text, Alert, TextInput, Keyboard, TouchableWithoutFeedback, Pressable, TouchableOpacity, StyleSheet } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -28,46 +30,166 @@ const Login = () => {
   }
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <View className="flex-1 justify-center items-center bg-gray-50 p-6">
-        <View className="w-full bg-white/50 backdrop-blur-md rounded-2xl p-8 shadow-lg">
-          <Text className="text-3xl font-bold mb-6 text-center text-gray-900">
-            Login
-          </Text>
-          <TextInput 
-            placeholder="Email"
-            placeholderTextColor="#6b7280"
-            className="border bg-gray-300 p-3 mb-4 rounded-xl"
-            value={email}
-            onChangeText={setEmail}
-          />
-          <TextInput 
-            placeholder="Password"
-            placeholderTextColor="#6b7280"
-            className="border bg-gray-300 p-3 mb-4 rounded-xl"
-            value={password}
-            onChangeText={setPassword}
-          />
+    <TouchableWithoutFeedback 
+      onPress={Keyboard.dismiss} 
+      accessible={false}
+    >
+      <SafeAreaView style={styles.container}>
+
+        <View style={styles.header}>
+          <View style={styles.headerIcon}>
+            <BookText size={40} color="#323232" />
+          </View>
+          <View style={styles.headerTextContainer}>
+            <Text style={styles.headerText1}>My</Text>
+            <Text style={styles.headerText2}>Library</Text>
+          </View>
+          <Text style={styles.subheaderText}>Welcome back! Please login to your account.</Text>
+        </View>
+
+        <View style={styles.formContainer}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputLabel}>Email Address</Text>
+            <View style={styles.inputField}>
+              <Mail size={16} color="#999" />
+              <TextInput 
+                style={styles.textInput}
+                value={email}
+                onChangeText={setEmail}
+              />
+            </View>
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputLabel}>Security Key</Text>
+            <View style={styles.inputField}>
+              <LockIcon size={16} color="#999" />
+              <TextInput 
+                style={styles.textInput}
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry
+              />
+              <Eye size={18} color="#999" />
+            </View>
+          </View>
           <Pressable 
-            className="bg-blue-600/80 px-6 py-3 rounded-xl"
+            style={styles.loginButton}
             onPress={handleLogin}
           >
-            <Text className="text-white text-lg text-center">Login</Text>
+            <Text style={styles.loginButtonText}>SIGN IN</Text>
           </Pressable>
-          <View className="flex-row justify-center mt-2">
-            <Text className="text-gray-600">
-              Do not have an account? 
+          <View style={styles.registerContainer}>
+            <Text style={styles.registerText}>
+              New to MyLibrary?
             </Text>
             <TouchableOpacity
+              style={styles.registerLink}
               onPress={() => router.push("/register")}
             >
-              <Text className="text-blue-600 font-semibold">Register</Text>
+              <Text style={styles.registerLinkText}>Register here</Text>
             </TouchableOpacity>
           </View>
         </View>
-      </View>
+      
+      </SafeAreaView>
     </TouchableWithoutFeedback>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  header: {
+    alignItems: "center",
+    marginTop: 40,
+    marginBottom: 20,
+  },
+  headerIcon: {
+    borderWidth: 1,
+    borderColor: "#323232",
+    borderStyle: 'dashed',
+    borderRadius: 50,
+    padding: 15,
+  },
+  headerTextContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  headerText1: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#000000",
+  },
+  headerText2: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#999999",
+  },
+  subheaderText: {
+    textAlign: "center",
+    fontSize: 14,
+    color: "#666",
+  },
+  formContainer: {
+    padding: 30,
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  inputLabel: {
+    fontSize: 14,
+    color: "#333",
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  inputField: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 2,
+  },
+  textInput: {
+    flex: 1,
+    height: 40,
+    marginLeft: 10,
+    fontSize: 14,
+  },
+  loginButton: {
+    backgroundColor: "#000",
+    paddingVertical: 12,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  loginButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  registerContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  registerText: {
+    fontSize: 14,
+    color: "#666",
+  },
+  registerLink: {
+    marginLeft: 5,
+  },
+  registerLinkText: {
+    fontSize: 14,
+    color: "#000",
+    fontWeight: "bold",
+    textDecorationLine: "underline",
+  },
+})
 
 export default Login;
--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,14 +1,19 @@
 import { router } from "expo-router"
 import { useState } from "react";
-import { View, Text, Alert, TextInput, TouchableWithoutFeedback, Keyboard, Pressable, TouchableOpacity } from "react-native"
+import { View, Text, Alert, TextInput, TouchableWithoutFeedback, Keyboard, Pressable, TouchableOpacity, StyleSheet } from "react-native"
 import { useLoader } from "@/hooks/useLoader";
 import { registerUser } from "@/services/authService";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { BookText, Eye, EyeOff, LockIcon, Mail, User } from "lucide-react-native";
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
   const { showLoader, hideLoader, isLoading } = useLoader();
 
@@ -34,58 +39,208 @@ const Register = () => {
   };
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
-      <View className="flex-1 justify-center items-center bg-gray-50 p-6">
-        <View className="w-full bg-white/50 backdrop-blur-md rounded-2xl p-8 shadow-lg">
-          <Text className="text-3xl font-bold mb-6 text-center text-gray-900">
-            Register
-          </Text>
-          <TextInput 
-            placeholder="Name"
-            placeholderTextColor="#6b7280"
-            className="border bg-gray-300 p-3 mb-4 rounded-xl"
-            value={name}
-            onChangeText={setName}
-          />
-          <TextInput 
-            placeholder="Email"
-            placeholderTextColor="#6b7280"
-            className="border bg-gray-300 p-3 mb-4 rounded-xl"
-            value={email}
-            onChangeText={setEmail}
-          />
-          <TextInput 
-            placeholder="Password"
-            placeholderTextColor="#6b7280"
-            className="border bg-gray-300 p-3 mb-4 rounded-xl"
-            value={password}
-            onChangeText={setPassword}
-          />
-          <TextInput 
-            placeholder="Confirm Password"
-            placeholderTextColor="#6b7280"
-            className="border bg-gray-300 p-3 mb-4 rounded-xl"
-            value={confirmPassword}
-            onChangeText={setConfirmPassword}
-          />
+    <TouchableWithoutFeedback 
+      onPress={Keyboard.dismiss} 
+      accessible={false}
+    >
+      <SafeAreaView style={styles.container}>
+
+        <View style={styles.header}>
+          <View style={styles.headerIcon}>
+            <BookText size={40} color="#323232" />
+          </View>
+          <View style={styles.headerTextContainer}>
+            <Text style={styles.headerText1}>My</Text>
+            <Text style={styles.headerText2}>Library</Text>
+          </View>
+          <Text style={styles.subheaderText}>Create an account to get started</Text>
+        </View>
+
+        <View style={styles.formContainer}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputLabel}>Full Name</Text>
+            <View style={styles.inputField}>
+              <User size={16} color="#999" />
+              <TextInput 
+                style={styles.textInput}
+                value={name}
+                onChangeText={setName}
+              />
+            </View>
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputLabel}>Email Address</Text>
+            <View style={styles.inputField}>
+              <Mail size={16} color="#999" />
+              <TextInput 
+                style={styles.textInput}
+                value={email}
+                onChangeText={setEmail}
+              />
+            </View>
+          </View>
+          <View style={styles.secureContainer}>
+            <View style={styles.secureInputContainer}>
+              <Text style={styles.inputLabel}>Security Key</Text>
+              <View style={styles.inputField}>
+                <LockIcon size={16} color="#999" />
+                <TextInput 
+                  style={styles.textInput}
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={!showPassword}
+                />
+                <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                  {showPassword ? (
+                    <EyeOff size={18} color="#999" />
+                  ) : (
+                    <Eye size={18} color="#999" />
+                  )}
+                </TouchableOpacity>
+              </View>
+            </View>
+            <View style={styles.secureInputContainer}>
+              <Text style={styles.inputLabel}>Confirm Key</Text>
+              <View style={styles.inputField}>
+                <LockIcon size={16} color="#999" />
+                <TextInput 
+                  style={styles.textInput}
+                  value={confirmPassword}
+                  onChangeText={setConfirmPassword}
+                  secureTextEntry={!showConfirmPassword}
+                />
+                {showConfirmPassword ? (
+                  <EyeOff size={18} color="#999" />
+                ) : (
+                  <Eye size={18} color="#999" />
+                )}
+              </View>
+            </View>
+          </View>
           <Pressable 
-            className="bg-blue-600/80 px-6 py-3 rounded-2xl"
+            style={styles.registerButton}
             onPress={handleRegister}
           >
-            <Text className="text-white text-lg text-center">Register</Text>
+            <Text style={styles.registerButtonText}>Register</Text>
           </Pressable>
-          <View className="flex-row justify-center mt-2">
-            <Text className="text-gray-700">Already have an account? </Text>
+          <View style={styles.loginContainer}>
+            <Text style={styles.loginText}>Already have an account? </Text>
             <TouchableOpacity
+              style={styles.loginLink}
               onPress={() => router.back()}
             >
-              <Text className="text-blue-600 font-semibold">Login</Text>
+              <Text style={styles.loginLinkText}>Sign In</Text>
             </TouchableOpacity>
           </View>
         </View>
-      </View>
+
+      </SafeAreaView>
     </TouchableWithoutFeedback>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  header: {
+    alignItems: "center",
+    marginTop: 40,
+    marginBottom: 20,
+  },
+  headerIcon: {
+    borderWidth: 1,
+    borderColor: "#323232",
+    borderStyle: 'dashed',
+    borderRadius: 50,
+    padding: 15,
+  },
+  headerTextContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  headerText1: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#000000",
+  },
+  headerText2: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#999999",
+  },
+  subheaderText: {
+    textAlign: "center",
+    fontSize: 14,
+    color: "#666",
+  },
+  formContainer: {
+    padding: 30,
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  inputLabel: {
+    fontSize: 14,
+    color: "#333",
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  inputField: {
+    flexDirection: "row",
+    alignItems: "center",
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 2,
+  },
+  textInput: {
+    flex: 1,
+    height: 40,
+    marginLeft: 10,
+    fontSize: 14,
+  },
+  secureContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  secureInputContainer: {
+    width: "48%",
+    marginBottom: 20,
+  },
+  registerButton: {
+    backgroundColor: "#000",
+    paddingVertical: 12,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  registerButtonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  loginContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  loginText: {
+    fontSize: 14,
+    color: "#666",
+  },
+  loginLink: {
+    marginLeft: 5,
+  },
+  loginLinkText: {
+    fontSize: 14,
+    color: "#000",
+    fontWeight: "bold",
+    textDecorationLine: "underline",
+  },
+})
 
 export default Register;
